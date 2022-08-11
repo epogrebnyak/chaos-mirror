0ea5abc2668e077bf034b8fbca3b8787f5f01110
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-08-11 15:30:47 +0300

    more content

--- justfile
+++ justfile
@@ -8,6 +8,14 @@ list:
 lint:
   - npx prettier . --write
 
+# Check documentation for errors
+check:
+  - mudkip build --check
+
+# Build HTML documentation
+build:
+  - mudkip build
+
 # Create LaTeX source files
 latex:
   - mudkip build --preset latex --output-dir docs/_latex
@@ -21,8 +29,3 @@ pdf:
 clean:
   - mudkip clean
   - rm -rf docs/_latex
-
-# Publish documentation to Github Pages 
-publish:
-  - mudkip build
-  - ghp-import -pfn docs/_build/dist  
---
+++ start.sh
@@ -0,0 +1,20 @@
+# This is a starter file for this repo, commands added manually.
+# WONTFIX: add environment config to container on start 
+#          as in https://aka.ms/configure-codespace
+conda install -c conda-forge just
+pip install update git+https://github.com/vberlier/mudkip.git
+pip install ghp-import
+
+# Install tons of latex:
+sudo apt-get install texlive-latex-base
+
+# Also install the recommended and extra fonts to avoid running into the error,
+# when trying to use pdflatex on latex files with more fonts.
+#sudo apt-get install texlive-fonts-recommended
+#sudo apt-get install texlive-fonts-extra
+
+# Install the extra packages
+# sudo apt-get install texlive-latex-extra
+
+# Hope it installs Russian for babel
+sudo apt-get install texlive-lang-cyrillic
