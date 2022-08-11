f976901a97aed04e7f34c72208e23e15c771c868
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-08-11 15:49:43 +0000

    must use sudo apt-get install texlive-latex-extra

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 # This is a starter file for this repo, commands added manually.
 # WONTFIX: add environment config to container on start 
-#          as in https://aka.ms/configure-codespace
+#          as in https://aka.ms/configure-codespace.
 conda install -c conda-forge just
 pip install update git+https://github.com/vberlier/mudkip.git
 pip install ghp-import
 
 # Install tons of latex:
 sudo apt-get install texlive-latex-base
+sudo apt-get install texlive-latex-extra
 sudo apt-get install texlive-lang-cyrillic
 #sudo apt-get install texlive-fonts-recommended
 #sudo apt-get install texlive-fonts-extra
-#sudo apt-get install texlive-latex-extra
-
